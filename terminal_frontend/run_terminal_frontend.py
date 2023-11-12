9632053a40130f70686257c2b97d661066bce6c2
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2023-11-11 22:26:25 -0800

    Terminal frontend uses new chancog method, but does not yet match items

--- terminal_frontend/run_terminal_frontend.py
+++ terminal_frontend/run_terminal_frontend.py
@@ -1,5 +1,5 @@
 from chancog.llm import OAIAzureServiceHandler
-from chancog.sagenerate.cosmos import CosmosHandler
+from chancog.cosmos import CosmosHandler
 from chancog.sagenerate.azfunc import smart_match_and_add
 from chancog.sagenerate.tvdb import TVDBHandler
 from chancog.sagenerate.openlibrary import OpenLibraryHandler
@@ -11,6 +11,7 @@ from chancog.entities import Conversation, Snippet
 import json
 from pprint import pprint
 from chancog.sagenerate.azfunc import rag_matcher
+from chancog.sagenerate.processing import process_new_user_message
 
 model_deployments = {
     'gpt-3.5-turbo': 'gpt-35-turbo-caeast', # Azure sometimes uses gpt-35-turbo
@@ -66,10 +67,10 @@ framing += "(e.g., title and author for a book)."
 greeting = "Hello, what can I suggest for you today?"
 
 # TODO: support entering and loading a conversation from a json file using a command line input
-conversation = Conversation()
-snippets = []
-conversation.add_snippet(Snippet('framing', framing))
-conversation.add_snippet(Snippet('greeting', greeting))
+#conversation = Conversation()
+#snippets = []
+#conversation.append_snippet(Snippet('framing', framing))
+#conversation.append_snippet(Snippet('greeting', greeting))
 
 # We use snippet_index to remember which snippets we've shown in the terminal frontend
 # In particular, it is the index in conversation.snippets of the next snippet we
@@ -77,6 +78,10 @@ conversation.add_snippet(Snippet('greeting', greeting))
 #snippet_index = 0
 
 def main():
+    conversation = Conversation()
+    conversation.append_snippet(Snippet('framing', framing))
+    conversation.append_snippet(Snippet('greeting', greeting))
+
     # We don't show the framing snippet, but do show the greeting snippet
     print('Assistant: ' + greeting + '\n')
     #snippet_index = 2
@@ -87,43 +92,63 @@ def main():
         if user_message.lower() == "exit":
             print("Exiting the chat bot...")
             break
-        conversation.add_snippet(Snippet('user_message', user_message))
-        #snippet_index += 1
 
-        #llm_message = lorem.sentence()
-        messages = conversation.build_open_ai_messages()
-        llm_message, _ = oai_handler_base.call_gpt(messages,
-                                                   model=dumb_model_base,
-                                                   json_mode=True)
-        try:
-            parsed_json = json.loads(llm_message)
-        except json.JSONDecodeError as e:
-            print("JSON decoding failed:", e)
-        except Exception as e:
-            raise(e)
-        print('Assistant: ' + parsed_json['text'] + '\n')
-        conversation.add_snippet(Snippet('llm_message', llm_message))
-        #snippet_index += 1
+        conversation, success = process_new_user_message(conversation,
+                                                         user_message,
+                                                         cosmos_handler,
+                                                         oai_handler_base,
+                                                         dumb_model_base,
+                                                         pc_handler,
+                                                         tvdb_handler,
+                                                         ol_handler)
 
-        # Loop over entries in new_items to do a Pinecone vector search
-        for suggestion in parsed_json['new_items']:
-            print('******************')
-            'LLM suggestion:'
-            pprint(suggestion)
-            item_info, diagnostics = rag_matcher(suggestion,
-                                                 oai_handler_base,
-                                                 oai_handler_azure,
-                                                 pc_handler,
-                                                 cosmos_handler.containers['items'],
-                                                 num_matches=3)
-            'Our match:'
-            pprint(item_info)
-            print('******************')
-    input_tokens, output_tokens = count_diagnostics_tokens(diagnostics)
-    cost = calc_gpt_cost(input_tokens, output_tokens, dumb_model_base)
-    print('Final cost:')
-    print(cost)
 
-       
+
+        llm_message = conversation.snippets[-1].text
+        print('**************')
+        parsed_json = json.loads(llm_message)
+        print('Assistant:\n')
+        pprint(llm_message)
+        print('\n')
+        #print('Assistant: ' + parsed_json + '\n')
+
 if __name__ == "__main__":
     main()
+#        conversation.append_snippet(Snippet('user_message', user_message))
+#        #snippet_index += 1
+#
+#        #llm_message = lorem.sentence()
+#        messages = conversation.build_open_ai_messages()
+#        llm_message, _ = oai_handler_base.call_gpt(messages,
+#                                                   model=dumb_model_base,
+#                                                   json_mode=True)
+#        try:
+#            parsed_json = json.loads(llm_message)
+#        except json.JSONDecodeError as e:
+#            print("JSON decoding failed:", e)
+#        except Exception as e:
+#            raise(e)
+#        print('Assistant: ' + parsed_json['text'] + '\n')
+#        conversation.append_snippet(Snippet('llm_message', llm_message))
+#        #snippet_index += 1
+#
+#        # Loop over entries in new_items to do a Pinecone vector search
+#        for suggestion in parsed_json['new_items']:
+#            print('******************')
+#            'LLM suggestion:'
+#            pprint(suggestion)
+#            item_info, diagnostics = rag_matcher(suggestion,
+#                                                 oai_handler_base,
+#                                                 oai_handler_azure,
+#                                                 pc_handler,
+#                                                 cosmos_handler.containers['items'],
+#                                                 num_matches=3)
+#            'Our match:'
+#            pprint(item_info)
+#            print('******************')
+#    input_tokens, output_tokens = count_diagnostics_tokens(diagnostics)
+#    cost = calc_gpt_cost(input_tokens, output_tokens, dumb_model_base)
+#    print('Final cost:')
+#    print(cost)
+
+       
